a3c97731a18d7a01b781c11b668b6fdb614a0a0a
Mark Ryall <mark@ryall.name>
2012-09-19 23:58:46 +1000

    added lookup method for phone number

--- lib/ocelots/cli.rb
+++ lib/ocelots/cli.rb
@@ -1,5 +1,22 @@
+require 'httparty'
+
 module Ocelots::Cli
   def self.execute *args
-    puts 'executing'
+    command, *args = *args
+    send command, *args
+  end
+
+  def self.phone email
+    persona_id = Digest::MD5.hexdigest email
+    response = request "profiles/#{persona_id}"
+    puts response['phone'] if response
+  end
+private
+  def self.request command
+    HTTParty.get "#{base_url}/api/#{command}?auth_token=#{ENV['AUTH_KEY']}"
+  end
+
+  def self.base_url
+    ENV['OCELOTS_URL'] || 'http://ocelots.herokuapp.com'
   end
 end
